9f2a3b7b4d1cd575a1f7f2311b1224857f489f3a
unknown <ceedriclazaro010205@gmail.com>
2024-09-06 22:25:48 -0800

    Initialize commit with app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+let Me = 19;
+let Brother = 24;
+
+let isMeOlderThanBrother = Me > Brother;
+let isMeYoungerThanBrother = Me < Brother;
+let isMeSameAgeAsBrother = Me == Brother;
+let isMeNotSameAgeAsBrother = Me !== Brother;
+
+console.log('Am I older than my brother?', isMeOlderThanBrother);
+console.log('Am I younger than my brother?', isMeYoungerThanBrother);
+console.log('Am I same age as my brother?', isMeSameAgeAsBrother);
+console.log('Am I not the same age as my brother?', isMeNotSameAgeAsBrother);
